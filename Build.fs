e95ae6652764bf59f7a3df9b728266dae29524c4
Martin Bryant <mail@martinbryant.dev>
2024-01-15 16:40:19 +0000

    tidy up the client scripts/remove webpack file

--- Build.fs
+++ Build.fs
@@ -23,7 +23,7 @@ Target.create "InstallClient" (fun _ -> run npm "install" ".")
 
 Target.create "Bundle" (fun _ ->
     [ "server", dotnet $"publish -c Release -o \"{deployPath}\"" serverPath
-      "client", dotnet "fable -o output --run npx vite build" clientPath ]
+      "client", dotnet "fable -o output --run npm run build" clientPath ]
     |> runParallel
 )
 
@@ -98,7 +98,7 @@ Target.create "Azure" (fun _ ->
 Target.create "Run" (fun _ ->
     run dotnet "build" sharedPath
     [ "server", dotnet "watch run" serverPath
-      "client", dotnet "fable watch -o output --run npx vite" clientPath ]
+      "client", dotnet "fable watch -o output --run npm run start" clientPath ]
     |> runParallel
 )
 
