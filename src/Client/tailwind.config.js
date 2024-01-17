294b6cf4afb7514f60859a1b8b24fbbc6f24510e
Martin Bryant <mail@martinbryant.dev>
2024-01-17 09:57:25 +0000

    get daisyUI/tailwind into the project

---
+++ src/Client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./index.html",
+      "./**/*.{fs,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
